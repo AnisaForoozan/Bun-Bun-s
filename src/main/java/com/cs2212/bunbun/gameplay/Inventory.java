b2070097a7b70130b26395fcd3701cb6168e6273
Anne Liu <aliu432@uwo.ca>
2024-11-21 00:40:09 -0500

    new inventory class

--- src/main/java/com/cs2212/bunbun/gameplay/Inventory.java
+++ src/main/java/com/cs2212/bunbun/gameplay/Inventory.java
@@ -1,28 +1,32 @@
 package com.cs2212.bunbun.gameplay;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Inventory extends JFrame implements ActionListener {
-    // Instance Variables
-    private float masterVolume;
-    private float musicVolume;
-    private float sfxVolume;
-    //private static AudioSettings instance;
+    // Instance variables
 
-    JButton saveGame;
     // Constructor
     public Inventory() {
         // Initialize the JFrame properties inside the constructor
-        saveGame = new JButton();
-        saveGame.setBounds(295, 100, 200, 50);
+        JLabel inventory = new JLabel("Inventory"); //create a label
+        inventory.setVerticalTextPosition(JLabel.TOP);
+        inventory.setHorizontalAlignment(JLabel.CENTER);
+        inventory.setVerticalAlignment(JLabel.CENTER);
 
-        this.setTitle("Settings"); // Set the title of the frame
+        // Create new JPanel to wrap the label
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 20));
+
+        // Create new frame
+        this.setTitle("Bun Bun's"); // Set the title of the frame
         this.setResizable(false);  // Prevent resizing of the frame
         this.setSize(800, 500); // Set the size of the frame
+        this.setLayout(new BorderLayout()); //new borderlayout
+        this.add(inventory, BorderLayout.NORTH); //set inventory label
         this.setVisible(true); // Make the frame visible
-        this.add(saveGame);
     }
 
     public static void main(String[] args) {
@@ -32,8 +36,8 @@ public class Inventory extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==saveGame) {
-
-        }
+//        if (e.getSource()==saveGame) {
+//
+//        }
     }
 }
